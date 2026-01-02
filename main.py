3cb3c930079cc0a060e93bcfb092e9e912a70b90
Rudnev <rudnnik@gmail.com>
2026-01-02 12:07:29 +0300

    feat: добавил логику сравнения и теорию про байты

--- main.py
+++ main.py
@@ -2,5 +2,9 @@
 name = input("Введите имя змеи: ")
 age = int(input("Введите возраст змеи (полных лет): "))
 weight = float(input("Введите вес змеи в граммах: "))
+is_heavy_enough = weight > 400
 
 print(f"Змея {name}: возраст {age} лет, вес {weight}г. Данные приняты.")
+print(f"Змея достаточно весит? {is_heavy_enough}")
+print(f"Тип переменной: {type(is_heavy_enough)}")
+print(type(1233))
